720c7e5a82c65948f0e22672d84a2e66ca5b562b
nve3pd <hoge.uchihara@gmail.com>
2018-01-23 23:10:22 +0900

    initial commit

---
+++ meow.nim
@@ -0,0 +1,43 @@
+import os
+import posix
+
+proc perror(s: cstring) {.header: "<stdio.h>", importc: "perror".}
+
+
+proc die(s: cstring): int {.discardable.} =
+  perror(s)
+  quit(1)
+
+
+proc do_cat(path: cstring): int {.discardable.} =
+  var 
+    fd: cint
+    n: int
+    buf: ptr cstring
+
+  fd = posix.open(path, O_RDONLY)
+  if fd < 0:
+    die(path)
+
+  while true:
+    n = posix.read(fd, buf.addr(), sizeof(buf))
+    if n < 0: die(path)
+    if n == 0: break
+    if posix.write(STDOUT_FILENO, buf.addr(), n) < 0:
+      die(path)
+
+  if posix.close(fd) < 0:
+    die(path)
+
+
+proc main(): int =
+  if os.paramCount() < 1:
+    echo "hoge"
+    quit(1)
+    
+  for i in countup(1, os.paramCount()):
+    do_cat(commandLineParams()[i - 1])
+
+
+if isMainModule:
+  discard main()
